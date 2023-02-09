eccdad58ce2e7602d33f1e63aa75cec0d3b95e9d
Mateusz Jamróz <jamr.mat@gmail.com>
2023-02-09 23:27:35 +0100

    api-schemas init

---
+++ api-schemas/src/test/java/trainwithme/schemas/apischemas/ApiSchemasApplicationTests.java
@@ -0,0 +1,13 @@
+package trainwithme.schemas.apischemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSchemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
